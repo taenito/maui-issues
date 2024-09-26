d1aef622fe94d1d522a31052ead077612bc1ef03
taenito <taenito@gmail.com>
2024-09-26 16:14:57 +0200

    Add MauiAppRefreshViewManuallyIndicatorHidden project

---
+++ MauiAppRefreshViewManuallyIndicatorHidden/MauiAppRefreshViewManuallyIndicatorHidden/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiAppRefreshViewManuallyIndicatorHidden;
+
+public partial class MainPage
+{
+    private MainPageModel _pageModel;
+    
+    public MainPage()
+    {
+        InitializeComponent();
+        BindingContext = _pageModel = new MainPageModel();
+    }
+}

2fd357514e719b4e382aedd38637bb2b88b4f40d
taenito <taenito@gmail.com>
2025-01-17 10:53:36 +0100

    Add CV2 test

--- MauiAppCollectionViewHeaderIsVisibleBindingWrongSize/MauiAppCollectionViewHeaderIsVisibleBindingWrongSize/MauiProgram.cs
+++ MauiAppCollectionViewHeaderIsVisibleBindingWrongSize/MauiAppCollectionViewHeaderIsVisibleBindingWrongSize/MauiProgram.cs
@@ -15,6 +15,14 @@ public static class MauiProgram
                 fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
             });
 
+#if IOS || MACCATALYST
+        builder.ConfigureMauiHandlers(handlers =>
+        {
+            //handlers.AddHandler<Microsoft.Maui.Controls.CollectionView, Microsoft.Maui.Controls.Handlers.Items2.CollectionViewHandler2>();
+            //handlers.AddHandler<Microsoft.Maui.Controls.CarouselView, Microsoft.Maui.Controls.Handlers.Items2.CarouselViewHandler2>();
+        });
+#endif
+        
 #if DEBUG
         builder.Logging.AddDebug();
 #endif
